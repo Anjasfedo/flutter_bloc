caae91bfd67ce3635f2500a03926746b5ca27d29
AnjasFedo <fedoaf98@gmail.com>
2024-05-17 23:35:52 +0700

    Init// Init Repo

---
+++ lib/models/task.dart
@@ -0,0 +1,11 @@
+class Task {
+  const Task({
+    required this.id,
+    required this.title,
+    required this.content,
+  });
+
+  final int id;
+  final String title;
+  final String content;
+}
